f727bef533d40a9e510bbc22f2b95518523957e5
Steve Kenworthy <swkenworthy@crossroadsint.org>
2010-07-10 22:14:51 +0800

    Fixed broken paths in build_cruise.sh

--- spec/build_cruise.sh
+++ spec/build_cruise.sh
@@ -1,9 +1,9 @@
 PASS_FAIL=0
 
-sudo gem install dbd-odbc --version="0.2.5"
-sudo gem install dbi --version="0.4.5"
-sudo gem install activeresource --version="2.3.5"
-cp config/config.yml.sample config/config_local.yml
+gem install dbd-odbc --version="0.2.5"
+gem install dbi --version="0.4.5"
+gem install activeresource --version="2.3.5"
+cp config.yml.sample config_local.yml
 
 spec --format progress --format html:$CC_BUILD_ARTIFACTS/rspec_report.html spec/
 if [ "$?" -eq "1" ]; then
