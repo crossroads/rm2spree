f1095760ce225bde537811d8e0b69edbb7fcf0af
Steve Kenworthy <swkenworthy@crossroadsint.org>
2010-07-14 16:33:28 +0800

    Removed gem setup for cruisecontrol.rb

--- spec/build_cruise.sh
+++ spec/build_cruise.sh
@@ -1,14 +1,7 @@
 PASS_FAIL=0
-
-gem install dbd-odbc --version="0.2.5"
-gem install dbi --version="0.4.5"
-gem install activeresource --version="2.3.5"
 cp config.yml.sample config_local.yml
-
 spec --format progress --format html:$CC_BUILD_ARTIFACTS/rspec_report.html spec/
 if [ "$?" -eq "1" ]; then
   PASS_FAIL=1
 fi
-
 exit $PASS_FAIL
-
