71c9b0b18c22cbfcd7aa77d51c92f33fa2185e3e
Nathan B <nathan.f77@gmail.com>
2010-07-17 12:27:43 +0800

    Fixed invalid product search script

--- script/find_invalid_web_products.rb
+++ script/find_invalid_web_products.rb
@@ -1,35 +1,30 @@
-require "odbc_spree_functions"
+require File.join("..", "lib", "odbc_spree.rb")
 
-Start_Time = Time.now
-$LOG = Logger.new("odbc_export.log", 10, 1024000)
-$LOG.formatter = Logger::Formatter.new
-$LOG.formatter.datetime_format = "%Y-%m-%d %H:%M:%S"
-$LOG.debug_x("\n\n -=- MYOB Database Synchronization Script started. -=- \n")
-
-# ------ Begin Code Execution -------
+@rm = RM.new("preview")
 
 @no_image = []
 @no_weight = []
 
-@stock_records_current = fetch_stock_records
+@stock_records_current = @rm.fetch_stock_records
 
 @stock_records_current.each do |stock_id, record|
-  if record["custom1"].downcase == "yes" 
+  if record["custom1"].downcase == "yes"
       @no_image << record["Barcode"] unless find_image(record["Barcode"])
       @no_weight << record["Barcode"] unless (record["custom2"].to_f > 0 and record["custom2"].to_f < 10)
   end
 end
 
-$LOG.debug_x(%Q"
+@rm.log.debug(%Q"
 
 :: Invalid web store products:
-     - No image count:  #{@no_image.size}    
-     - No weight count: #{@no_weight.size}  
-     - Total: #{@no_image.size + @no_weight.size}    
+     - No image count:  #{@no_image.size}
+     - No weight count: #{@no_weight.size}
+     - Total: #{@no_image.size + @no_weight.size}
 ")
 
-$LOG.debug_x " ---- barcodes without images ---- "
-$LOG.debug_x @no_image.sort.collect{|x| "'#{x}'"}.join(", ")
+@rm.log.debug " ---- barcodes without images ---- "
+@rm.log.debug @no_image.sort.collect{|x| "'#{x}'"}.join(", ")
 
-$LOG.debug_x "\n\n\n ---- barcodes without weights ---- "
-$LOG.debug_x @no_weight.sort.collect{|x| "'#{x}'"}.join(", ")
+@rm.log.debug "\n\n\n ---- barcodes without weights ---- "
+@rm.log.debug @no_weight.sort.collect{|x| "'#{x}'"}.join(", ")
+
