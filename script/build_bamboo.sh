8237816fa0b1884988262260fbcf6215265e23aa
Nathan Broadbent <nathan.f77@gmail.com>
2011-05-18 13:43:15 +0800

    Fix bamboo build script for RVM

--- script/build_bamboo.sh
+++ script/build_bamboo.sh
@@ -1,7 +1,9 @@
 # Project variables
 # -----------------------------------------------------
-project_name=rm2spree
-ruby_version=1.8.7-p330
+application=rm2spree
+ruby_version=ruby-1.8.7-p302
+rubygems_version=1.3.7
+bundler_version=1.0.9
 
 ruby_packages="ruby ruby-devel gcc rubygems"
 cucumber_packages="libxml2 libxml2-devel libxslt libxslt-devel xorg-x11-server-Xvfb firefox ImageMagick"
@@ -19,14 +21,32 @@ fi
 
 # Set up RVM as a function. This loads RVM into a shell session.
 # -----------------------------------------------------
-[[ -s "$HOME/.rvm/src/rvm/scripts/rvm" ]] && . "$HOME/.rvm/src/rvm/scripts/rvm"
+. "/usr/local/rvm/scripts/rvm"
 
-# Install and use the configured ruby version
+# Set up ruby environment
 # -----------------------------------------------------
-if ! (rvm list | grep $ruby_version); then rvm install $ruby_version; fi;
-
-# Set up and use project gemset for ruby version
-rvm use $ruby_version
+# Install and use the configured ruby version
+if ! (rvm list | grep $ruby_version); then
+    rvm install $ruby_version
+fi
+# Find/Create project gemset, and use it.
+if ! (rvm list gemsets | grep "$ruby_version@$application "); then
+    rvm use $ruby_version
+    rvm gemset create "$application"
+fi
+rvm use "$ruby_version@$application"
+# Install and use correct version of rubygems
+if ! (gem -v | grep "$rubygems_version"); then
+    # Only rubygems ~>1.6.2 can be downgraded.
+    rvm rubygems 1.6.2
+    if ! (gem -v | grep "$rubygems_version"); then
+        gem update --system $rubygems_version
+    fi
+fi
+# Install and use correct version of bundler.
+if ! (gem list | grep "bundler" | grep $bundler_version); then
+    gem install bundler -v=$bundler_version --no-rdoc --no-ri
+fi
 
 # Install Bundle
 # -----------------------------------------------------
